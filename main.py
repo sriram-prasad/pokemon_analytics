cb1f9e84413ee01a9918858fc8fd0816b26c8f59
Sriram Prasad <sriramprasad2015@gmail.com>
2023-06-27 00:44:48 +1000

    Added abilities to data/, updated .py files

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from extraction import Extraction
 
 def main():
     ext_obj = Extraction()
-    list(map(ext_obj.extract_data, ["pokemon", "type", "move"]))
+    list(map(ext_obj.extract_data, ["pokemon", "type", "move", "ability"]))
 
 
 if __name__ == "__main__":
