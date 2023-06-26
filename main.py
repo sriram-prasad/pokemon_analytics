c899e3cf763eec92783bc3099472b8bff7bab504
Sriram Prasad <sriramprasad2015@gmail.com>
2023-06-26 20:09:56 +1000

    Initialised main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from extraction import Extraction
+
+
+def main():
+    ext_obj = Extraction()
+    ext_obj.extract_pokemon()
+    ext_obj.extract_types()
+
+
+if __name__ == "__main__":
+    main()
