63ff258228abe1aa5b1e05c390e4ee77384a4977
Sriram Prasad <sriramprasad2015@gmail.com>
2023-06-29 12:11:10 +1000

    Removed item line item

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from extraction import Extraction
 
 def main():
     ext_obj = Extraction()
-    ext_obj.extract_data("item")
-    # list(map(ext_obj.extract_data, ["pokemon", "type", "move", "ability", "item"]))
+    list(map(ext_obj.extract_data, ["pokemon", "type", "move", "ability", "item"]))
 
 
 if __name__ == "__main__":
